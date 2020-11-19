f95e8e74df9e4f0aad3ef6fbe26b12f378758d5b
godofsleepy <45101689+godofsleepy@users.noreply.github.com>
2020-11-19 21:46:10 +0700

    update layout profile

--- lib/view/profile/profile_view.dart
+++ lib/view/profile/profile_view.dart
@@ -33,53 +33,66 @@ class _ProfileViewState extends State<ProfileView> {
                     ),
                   ),
                 ),
-                SizedBox.expand(
-                  child: DraggableScrollableSheet(
-                    initialChildSize: 0.7,
-                    minChildSize: 0.7,
-                    builder: (context, scrollController) {
-                      return SingleChildScrollView(
-                        controller: scrollController,
-                        child: Container(
-                          width: MediaQuery.of(context).size.width + 75,
-                          height: MediaQuery.of(context).size.height,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(30),
-                              topLeft: Radius.circular(30),
-                            ),
+                SingleChildScrollView(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.3),
+                        padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.08,
+                          left: 2,
+                          right: 2,
+                        ),
+                        // height: 500,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(24),
+                            topRight: Radius.circular(24),
                           ),
-                          child: Padding(
-                            padding:
-                                EdgeInsets.only(top: 24, right: 18, left: 18),
-                            child: ListView(
-                              padding: EdgeInsets.zero,
-                              controller: scrollController,
-                              children: [
-                                Column(
-                                  children: [
-                                    Container(
-                                      height: 100,
-                                      width: 100,
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(20),
-                                        child: Image.network(
-                                          "https://images.unsplash.com/photo-1597653787129-6557d1ddcb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=938&q=80",
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Text("Rifat Khadafy", style: TextStyle(
+                              color: ResColor.blackColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 150,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.network(
+                                "https://images.unsplash.com/photo-1509585585779-17594514ad43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+                                fit: BoxFit.cover,
+                                width: 90,
+                                height: 90,
+                              ),
                             ),
                           ),
                         ),
-                      );
-                    },
+                      )
+                    ],
                   ),
-                )
+                ),
               ],
             ),
           ),
