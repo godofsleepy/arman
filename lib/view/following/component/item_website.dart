5a722ee24d0c507b603f320a71987ea04a4ffb85
godofsleepy <45101689+godofsleepy@users.noreply.github.com>
2020-11-23 16:40:31 +0700

    update following

---
+++ lib/view/following/component/item_website.dart
@@ -0,0 +1,31 @@
+import 'package:arman/helper/resource.dart';
+import 'package:flutter/material.dart';
+
+class ItemWebsite extends StatelessWidget {
+  const ItemWebsite({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 90,
+      decoration: BoxDecoration(
+          color: ResColor.greyColor, borderRadius: BorderRadius.circular(20)),
+      margin: EdgeInsets.all(6),
+      child: Column(
+        children: [
+          Container(
+            child: ClipRRect(
+              child: Image.network(
+                "https://rumaysho.com/wp-content/uploads/2017/04/cropped-Master-Logo_Logo-Icon-Rumaysho-Merah_transparan.png",
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Text("rumaysho")
+        ],
+      ),
+    );
+  }
+}
--- lib/view/following/following_view.dart
+++ lib/view/following/following_view.dart
@@ -1,3 +1,6 @@
+import 'package:arman/helper/resource.dart';
+import 'package:arman/view/following/component/item_website.dart';
+import 'package:chips_choice/chips_choice.dart';
 import 'package:flutter/material.dart';
 
 class FollowingView extends StatefulWidget {
@@ -8,10 +11,82 @@ class FollowingView extends StatefulWidget {
 }
 
 class _FollowingViewState extends State<FollowingView> {
+  List<String> tags = [];
+  List<String> options = [
+    'News',
+    'Entertainment',
+    'Politics',
+    'Automotive',
+    'Sports',
+    'Education',
+    'Fashion',
+    'Travel',
+    'Food',
+    'Tech',
+    'Science',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 50, top: 20, left: 20, right: 20),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Website",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: ResColor.blackColor.withOpacity(0.8),
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 15),
+                  width: MediaQuery.of(context).size.width,
+                  height: 330,
+                  child: GridView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 9,
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (BuildContext context, int index) =>
+                        ItemWebsite(),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Topik",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: ResColor.blackColor.withOpacity(0.7),
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                ChipsChoice<String>.multiple(
+                  value: tags,
+                  wrapped: true,
+                  onChanged: (val) => setState(() => tags = val),
+                  choiceItems: C2Choice.listFrom<String, String>(
+                    source: options,
+                    value: (i, v) => v,
+                    label: (i, v) => v,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
